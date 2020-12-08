000875ffbbbce38c414b734f58dcfdc8a788e5b4
Ellie Lambert <ebezjian@gmail.com>
2020-12-08 10:45:27 -0500

    finished

--- Calendar.Tests/ModelTests/LeapYearTests.cs
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -9,8 +9,28 @@ namespace Calendar.Tests
     [TestMethod]
     public void IsLeapYear_NumberDivisibleByFour_True()
     {
-      //testing code will go here
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
     }
+    [TestMethod]
+    public void IsLeapYear_NumberNotDivisibleByFour_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
+    }
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfOneHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+    }
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfFourHundred_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
+    }
+
   }
 }
 

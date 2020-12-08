b7de850c100650be2656d0a0c2124d0231f3e43c
Ellie Lambert <ebezjian@gmail.com>
2020-12-08 10:19:53 -0500

    started basic testing structure

---
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+
+namespace Calendar.Tests
+{
+  [TestClass]
+  public class LeapYearTests
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      //testing code will go here
+    }
+  }
+}
+
+
+
+
